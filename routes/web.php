668f7cef978381ccffe987422fd69038fa24a1fe
hklrahmadi <haikalrahmadi@kecilin.id>
2025-03-06 03:45:08 +0700

    update webhook

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,21 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
+Route::post('/git-webhook', function () {
+    try {
+        $output = shell_exec('cd /home/ruat9133/repositories/ruangjiwadanadyaksa && git pull origin main && composer install --no-dev --prefer-dist && php artisan migrate --force 2>&1');
+
+        Log::info('Git Deploy Output:', [$output]);
+
+        return response()->json(['message' => 'Repository updated!', 'output' => $output]);
+    } catch (\Exception $e) {
+        Log::error('Git Webhook Error: ' . $e->getMessage());
+        return response()->json(['error' => 'Failed to update repository'], 500);
+    }
+});
+
+
+
 Route::get('/', [FrontController::class, 'index'])->name('vote.index');
 Route::post('/vote', [FrontController::class, 'vote'])->name('vote.store');
 Route::get('/artworks/{artwork:slug}', [FrontController::class, 'show'])->name('artworks.show');
