588cfcd6154ae93ab271d2322afe5a0b6e360e86
hklrahmadi <haikalrahmadi@kecilin.id>
2025-03-10 21:17:17 +0700

    feat - fix responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="min-h-screen bg-gradient-to-br from-amber-50 to-yellow-100">
+<body class="min-h-screen overflow-x-hidden bg-gradient-to-br from-amber-50 to-yellow-100">
     {{-- <div class="bg-gradient-to-r from-amber-400 to-yellow-500 shadow-lg">
         <div class="max-w-6xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between items-center">
